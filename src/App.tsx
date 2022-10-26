23a363b97ebd9e5a86bc6762cc13affd0ca3d5ba
ben <me@benpaat.xyz>
2022-10-26 22:16:20 +0800

    fix: add cleanup func to make zooming in and out smooth

--- src/App.tsx
+++ src/App.tsx
@@ -36,10 +36,11 @@ const randomHexColor = () => {
 };
 
 const Home = () => {
-  const [colorCardSize, setColorCardSize] = useState({
+  const colorCardSize = {
     height: 90,
     width: 90,
-  });
+  };
+
   const [colors, setColors] = useState<string[]>([]);
   const [windowDimension, setWindowDimension] = useState({
     width: window.innerWidth,
@@ -64,6 +65,9 @@ const Home = () => {
 
   useEffect(() => {
     window.addEventListener("resize", handleResize, false);
+    return () => {
+      window.removeEventListener("resize", handleResize, false);
+    };
   }, []);
 
   useEffect(() => {
